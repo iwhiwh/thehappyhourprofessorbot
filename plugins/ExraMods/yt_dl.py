914dfe0b8837eb5712e131aba4c67314638f642d
iwhiwh <131843374+iwhiwh@users.noreply.github.com>
2023-07-27 18:28:43 +0530

    Update yt_dl.py

--- plugins/ExraMods/yt_dl.py
+++ plugins/ExraMods/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [The Happy Hour ™](https://t.me/The_Happy_Hour_Hindi)**"
+        cap = "**BY›› [The Happy Hour™](https://t.me/The_Happy_Hour_Hindi)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
